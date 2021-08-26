6965e303b49a7889f31d4e7d4148785a2e2e816f
Kyle Lynch <kylelynch104@gmail.com>
2021-08-26 16:58:12 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useRef } from 'react'
+import { createStage, isColliding } from './gameHelpers'
+
+// Custom hooks
+import { useInterval } from './hooks/useInterval'
+import { usePlayer } from './hooks/usePlayer'
+import { useStage } from './hooks/useStage'
+
+// Components
+import Stage from './components/Stage/Stage'
+import Display from './components/Display/Display'
+import StartButton from './components/StartButton/StartButton'
+
+// Styles
+import { StyledTetrisWrapper, StyledTetris } from './App.styles'
+
+const App: React.FC = () => {
+  const [dropTime, setDropTime] = useState<null | number>(null)
+  const [gameOver, setGameOver] = useState(true)
+
+  const gameArea = useRef<HTMLDivElement>(null)
+
+  const { player, updatePlayerPos, resetPlayer, playerRotate } = usePlayer()
+  const { stage, setStage } = useStage(player, resetPlayer)
+
+  const movePlayer = (dir: number) => {
+    if (!isColliding(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0, collided: false })
+    }
+  }
+
+  const keyUp = ({ keyCode }: { keyCode: number }): void => {
+    // Change the droptime speed when user releases down arrow
+    if (keyCode === 40) {
+      setDropTime(1000)
+    }
+  }
+
+  const handleStartGame = (): void => {
+    // Need to focus the window with the key events on start
+    if (gameArea.current) gameArea.current.focus()
+    // Reset everything
+    setStage(createStage())
+    setDropTime(1000)
+    resetPlayer()
+    setGameOver(false)
+  }
+
+  const move = ({
+    keyCode,
+    repeat,
+  }: {
+    keyCode: number
+    repeat: boolean
+  }): void => {
+    if (keyCode === 37) {
+      movePlayer(-1)
+    } else if (keyCode === 39) {
+      movePlayer(1)
+    } else if (keyCode === 40) {
+      // Just call once
+      if (repeat) return
+      setDropTime(30)
+    } else if (keyCode === 38) {
+      playerRotate(stage)
+    }
+  }
+
+  const drop = (): void => {
+    if (!isColliding(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false })
+    } else {
+      // Game over!
+      if (player.pos.y < 1) {
+        console.log('game over')
+        setGameOver(true)
+        setDropTime(null)
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true})
+    }
+  }
+
+  useInterval(() => {
+    drop()
+  }, dropTime)
+
+  return (
+    <StyledTetrisWrapper
+      role='button'
+      tabIndex={0}
+      onKeyDown={move}
+      onKeyUp={keyUp}
+      ref={gameArea}
+    >
+      <StyledTetris>
+        <div className='display'>
+          {gameOver ? (
+            <>
+              <Display gameOver={gameOver} text='Game Over!' />
+              <StartButton callback={handleStartGame} />
+            </>
+          ) : (
+            <>
+              <Display text={`Score: `} />
+              <Display text={`Rows: `} />
+              <Display text={`Level: `} />
+            </>
+          )}
+        </div>
+        <Stage stage={stage} />
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  )
+}
+
+export default App
---
+++ src/hooks/useStage.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { createStage } from '../gameHelpers'
+
+//  Types
+import type { PLAYER } from './usePlayer'
+
+export type STAGECELL = [string | number, string]
+export type STAGE = STAGECELL[][]
+
+export const useStage = (player: PLAYER, resetPlayer: () => void) => {
+  const [stage, setStage] = useState(createStage())
+
+  useEffect(() => {
+    if (!player.pos) return
+
+    const updateStage = (prevStage: STAGE): STAGE => {
+      //  first flush the stage
+      //  if it says clear but don't have a 0, it means that it's the players move and should be cleared
+      const newStage = prevStage.map(
+        row =>
+          row.map(cell =>
+            cell[1] === 'clear' ? [0, 'clear'] : cell
+          ) as STAGECELL[]
+      )
+
+      //    then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ]
+          }
+        })
+      })
+
+      if (player.collided) {
+        resetPlayer()
+      }
+
+      return newStage
+    }
+
+    setStage(prev => updateStage(prev))
+  }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino])
+
+  return { stage, setStage }
+}
